5203ea174a94e932c1e6cbd795ea5b4481a55bfa
albo95 <albobruno95@gmail.com>
2024-03-25 11:33:39 +0100

    First commit

---
+++ SimpleChatGPTApp/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SimpleChatGPTApp
+//
+//  Created by Alberto Bruno on 25/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Simple ChatGPT app")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleChatGPTApp/SimpleChatGPTAppApp.swift
@@ -0,0 +1,31 @@
+//
+//  SimpleChatGPTAppApp.swift
+//  SimpleChatGPTApp
+//
+//  Created by Alberto Bruno on 25/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleChatGPTAppApp: App {
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            Item.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
